10a53656c5661010b6270b6675ebda01f84ea561
Jakob <jholterh@student.42berlin.de>
2025-08-05 17:05:09 +0200

    feat: added promo under the copy link field

--- src/components/StartScreen.tsx
+++ src/components/StartScreen.tsx
@@ -68,6 +68,7 @@ export function StartScreen() {
                   </button>
                 </div>
               </div>
+              <PromoSection />
             </div>
           </div>
         </main>
